dbb5dbcdd12788e18fbb5f8e465c1640378aeea3
mo khan <mo@mokhan.ca>
2017-01-30 09:03:07 -0700

    initial commit.

---
+++ lib/capistrano/s3tarball.rb
@@ -0,0 +1,7 @@
+require "capistrano/s3tarball/version"
+
+module Capistrano
+  module S3tarball
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/s3tarball/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module S3tarball
+    VERSION = "0.1.0"
+  end
+end
